f1d989bb9962204870508bf03d8cb462ebea3855
Rezwana-Nazri <81165583+Rezwana-Nazri@users.noreply.github.com>
2021-11-18 02:42:49 +0600

    new java class added

---
+++ app/src/main/java/com/project/newsapp/activities/Nazri.java
@@ -0,0 +1,17 @@
+package com.project.newsapp.activities;
+
+public class Nazri {
+    int x;
+
+    public Nazri(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
